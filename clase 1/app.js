dcbf53a4c71e0a5a36e95178c7edf77485dd5ddf
jjoaco <89048172+jjoaco@users.noreply.github.com>
2021-09-15 01:46:52 -0300

    Add files via upload

---
+++ clase 1/app.js
@@ -0,0 +1,10 @@
+let nombre = prompt("Por favor ingresa tu nombre");
+let apellido = prompt("Por favor ingresa tu apellido");
+        
+    let ingresado  = nombre + " " + apellido +  " " + "su nombre y apellido fue ingresado.";
+    alert(ingresado);
+
+
+console.log(nombre);
+console.log(apellido);
+
