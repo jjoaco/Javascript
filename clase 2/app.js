d4753a6db0c38686578b8dad0c4ccde84870d170
jjoaco <89048172+jjoaco@users.noreply.github.com>
2021-09-16 21:46:55 -0300

    Add files via upload

---
+++ clase 2/app.js
@@ -0,0 +1,12 @@
+let numero = Number(prompt("Escribe un número del 1 al 10 si adivinas ganás"))
+
+if(numero ===8) {
+    alert("¡¡Ganaste Felicidades!!");
+    } else if(numero >=11 ) {
+        alert ("¿Te crees gracios@? Te dije del 1 al 10")
+     } else if(numero ===0 ) {
+            alert ("¿Te crees gracios@? Te dije del 1 al 10")
+            }  else if(numero >=1 ) {
+                alert ("Perdiste")
+                }
+    
