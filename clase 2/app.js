a9aabeb8496e3068a3f74af0585bf12d233f2323
jjoaco <89048172+jjoaco@users.noreply.github.com>
2021-09-16 22:15:57 -0300

    Update app.js

--- clase 2/app.js
+++ clase 2/app.js
@@ -3,10 +3,13 @@ let numero = Number(prompt("Escribe un número del 1 al 10 si adivinas ganás"))
 if(numero ===8) {
     alert("¡¡Ganaste Felicidades!!");
     } else if(numero >=11 ) {
-        alert ("¿Te crees gracios@? Te dije del 1 al 10")
-     } else if(numero ===0 ) {
+         alert ("¿Te crees gracios@? Te dije del 1 al 10")
+      } else if(numero ===0 ) {
             alert ("¿Te crees gracios@? Te dije del 1 al 10")
             }  else if(numero >=1 ) {
                 alert ("Perdiste")
                 }
-    
+
+else{(numero)
+    alert("Tenes que poner un numero")
+}    
