f45b29b4ac0bbbe01e8716237b5a1cf6119ee53e
jjoaco <89048172+jjoaco@users.noreply.github.com>
2021-09-23 17:59:59 -0300

    Add files via upload

---
+++ clase 3/app.js
@@ -0,0 +1,8 @@
+let num = Number(prompt('Ingresa un numero y voy a repetirte por consola un "Hola" la cantidad de veces del numero que pongas.'))
+for(let i = 1; i <=num; i++) {
+    
+    if(i) {
+        console.log('"Hola" repiticion numero' +i);
+    }
+
+}
